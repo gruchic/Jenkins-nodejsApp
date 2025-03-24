6a576b5f9e0e6d41fdf2dce8a8455732e3e85f42
gru <gurpreet.singh@chicmicstudios.in>
2025-03-24 15:30:38 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (request, response) => {
+  response.end('Hello from Node.js pipeline!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error:', err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
+
---
+++ test.js
@@ -0,0 +1,7 @@
+// test.js
+const assert = require('assert');
+
+// Example test: check that 2+2 equals 4
+assert.strictEqual(2 + 2, 4, 'Math is broken!');
+console.log('All tests passed!');
+
